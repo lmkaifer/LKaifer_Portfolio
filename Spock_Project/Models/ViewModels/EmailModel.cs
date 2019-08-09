83ee18dc4af92277e8ead598a93b5f6f442ff036
lmkaifer <52322859+lmkaifer@users.noreply.github.com>
2019-08-09 06:38:34 -0400

    Portfolio Update
    Portfolio Update

--- Spock_Project/Models/ViewModels/EmailModel.cs
+++ Spock_Project/Models/ViewModels/EmailModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace Spock_Project.ViewModels
+namespace Spock_Project.Models.ViewModels
 {
     public class EmailModel
     {
